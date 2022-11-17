7c587f9e3a3a79945003b9065aa04efe3ee40116
Ashish131294 <ashish.kachhadiya1114@gmail.com>
2022-11-17 08:39:26 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:form/viewpage.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: viewpage(),
+  ));
+}
+
+class form extends StatefulWidget {
+  const form({Key? key}) : super(key: key);
+
+  @override
+  State<form> createState() => _formState();
+}
+
+class _formState extends State<form> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
